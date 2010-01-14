a80a8fc1334c2aab3d6ada1f0a43e466355db3b5
Julio Carlos Menendez <godinblack@gmail.com>
2010-01-13 21:58:55 -0700

    Adds main styles file. Servers static files with django.

--- extensionhub/urls.py
+++ extensionhub/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,4 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
 )
